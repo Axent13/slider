769fc4c298aded15fc8d554cbcae6988c2fd9098
Axent13 <axent13@yandex.ru>
2021-01-11 20:08:16 +0700

    Перемещение левой границы по клику

--- src/slider/slider.ts
+++ src/slider/slider.ts
@@ -12,6 +12,21 @@ class Slider {
     this.$rangeLine = $(this.$rootElement).find('.js-slider__range-line');
     this.$startPointElement = $(this.$rootElement).find('.js-slider__point_position_start');
     this.$endPointElement = $(this.$rootElement).find('.js-slider__point_position_end');
+
+    this._initEventListeners();
+  }
+
+  _handleSliderClick(event: any) {
+    // странный код, но здесь мне мешает ошибка - Object is possibly 'undefined'.ts(2532)
+    // постараюсь переписать позже
+    const shiftX = $(this.$rootElement)?.offset()?.left || 0;
+    const newPos = event.pageX - shiftX;
+
+    $(this.$rangeLine).css('left', `${newPos}px`);
+  }
+
+  _initEventListeners() {
+    $(this.$rootElement).on('mousedown', this._handleSliderClick.bind(this));
   }
 }
 
