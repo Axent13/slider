046d10760728224002335673e3dc68a17db41c09
Axent13 <axent13@yandex.ru>
2021-02-17 20:50:05 +0700

    Убрал из Модели методы Обзервера

--- src/slider/model.js
+++ src/slider/model.js
@@ -56,20 +56,6 @@ class Model extends Observer {
     this.endSelectedValue = newValue;
     this.emit({ type: 'modelUpdatedEndSelectedValue', data: this.endSelectedValue });
   }
-
-  subscribe(observer) {
-    this.observers.push(observer);
-  }
-
-  unsubscribe(observer) {
-    this.observers = this.observers.filter((currentObserver) => currentObserver !== observer);
-  }
-
-  emit({ type, data }) {
-    this.observers.forEach((currentObserver) => {
-      currentObserver.update({ type, data });
-    });
-  }
 }
 
 export default Model;
